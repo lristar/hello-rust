0aabd31f41e0dde18848dc6f15b6c9c266285d4e
Lizhuyou <84826607@qq.com>
2022-02-05 15:34:54 +0800

    面向对象封装

--- src/eight_collector/test_hashmap.rs
+++ src/eight_collector/test_hashmap.rs
@@ -1,19 +1,31 @@
 use std::collections::HashMap;
 #[derive(Debug)]
 struct HM {
-    pub h: HashMap<i32, String>,
+    h: HashMap<i32, String>,
 }
 
 impl HM {
+    fn test_new() -> HM {
+        let mut hm = HashMap::new();
+        HM { h: (hm) }
+    }
     fn test_get_value(&self, s: i32) -> String {
-        let a = self.h.get(&s).unwrap().clone();
-        return a;
+        let default = String::from("");
+        let a = self.h.get(&s).unwrap_or(&default);
+        return a.to_string();
     }
 
     // Todo 怎么解决这个所有权问题
-    fn test_del_value(mut self, s: i32) -> HM {
+    fn test_del_value(&mut self, s: i32) {
         self.h.remove(&s);
-        return self;
+    }
+    fn test_add_value(&mut self, k: i32, v: String) {
+        self.h.insert(k, v);
+    }
+    fn test_get_all(&self) {
+        for kv in self.h.iter() {
+            println!("kv : {:?}", kv);
+        }
     }
 }
 
@@ -32,9 +44,15 @@ fn test_del_value() {}
 // 你可以指定一个不同的 hasher 来切换为其它函数。hasher 是一个实现了 BuildHasher trait 的类型
 #[test]
 fn test_hash_map() {
-    let hm = test_add_value();
+    let mut hm = HM::test_new();
     let _aaa = hm.test_get_value(13);
+    if _aaa == "".to_string() {
+        println!("_aaa is{}", _aaa);
+    }
     // 内部如果修改了结构体需要重新给予所有权，要不然所有权进去了基本在方法内就消失了。
-    let hh = hm.test_del_value(13);
-    println!("hm:{:#?}", hh);
+    hm.test_add_value(15, "15".to_string());
+    hm.test_del_value(15);
+    hm.test_add_value(16, "16".to_string());
+    hm.test_get_all();
+    println!("hm:{:#?}", hm);
 }
