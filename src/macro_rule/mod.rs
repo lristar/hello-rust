1479de51882e5af0487bf1d81ef665c04872cdfa
Lizhuyou <84826607@qq.com>
2022-02-19 14:29:42 +0800

    修改结构

--- src/macro_rule/mod.rs
+++ src/macro_rule/mod.rs
@@ -1,12 +1,1 @@
-macro_rules! say_hello {
-    // `()` 表示此宏不接受任何参数。
-    () => (
-        // 此宏将会展开成这个代码块里面的内容。
-        println!("Hello!");
-    )
-}
-
-#[test]
-fn test(){
-    say_hello!()
-}
+pub mod test_macro_rule;
---
+++ src/test/mod.rs
@@ -0,0 +1,16 @@
+extern crate rand;
+pub mod eight_collector;
+pub mod eleven_autotest;
+pub mod fifteen_smart_point;
+pub mod five_struct;
+pub mod four_owner;
+pub mod leetcode_test;
+pub mod nine_error;
+pub mod nineteen_top_level;
+pub mod seventeen_oop;
+pub mod six_em;
+pub mod sixteen_thread;
+pub mod ten_genericity_trait_;
+pub mod thirteen_iters;
+pub mod twelve_cmd;
+pub mod two_three_example;
